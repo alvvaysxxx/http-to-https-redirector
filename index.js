e2c31830c52b3b2f536403295495e03344eff02f
alvvaysxxx <alvvaysxxx@gmail.com>
2024-02-08 21:30:05 +0300

    1

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET", "POST");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type,Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+app.get("/*", async (req, res) => {
+  try {
+    console.log(req.headers);
+    let response = await axios.get(`http://${req.path}`, {
+      headers: req.headers,
+    });
+    res.status(response.status).json(response.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: true });
+  }
+});
+
+app.post("/*", async (req, res) => {
+  try {
+    console.log(req.headers);
+    let response = await axios.post(`http://${req.path}`, req.body, {
+      headers: req.headers,
+    });
+    res.status(response.status).json(response.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: true });
+  }
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  try {
+    console.log(`Redirector started on port ${PORT}.`);
+  } catch (err) {
+    console.error(err);
+  }
+});
